0ac1146ba55d0bf7e3e45bc1b0217a6837441a7b
jacobhjustice <jacobhj1997@gmail.com>
2020-05-26 21:56:15 -0500

    k choice

--- app/ts/viewManager.ts
+++ app/ts/viewManager.ts
@@ -6,25 +6,27 @@ const processedImage = require("./ts/generated/processedImage");
 class ViewManager {
 
     img: ProcessedImage
-
+    path: string;
+    k: number;
     constructor() {
         this.img = new processedImage.ProcessedImage("", [], 0, 0);
+        this.path = "";
+        this.k = 0;
     }
 
-    OnChange_FileUpload(files: any) {
+    OnChange_FileUpload() {
         
         const getPixels = require("get-pixels");
         var parse = (pixels: number[], w: number, h: number) => {
-            this.img = imageParser.ImageParser.ParseImage(pixels, w, h, 5);
+            this.img = imageParser.ImageParser.ParseImage(pixels, w, h, this.k);
             this.img.WriteImage(this.displayImg)
         }
-        if(files.length > 0) {
-            getPixels(files[0].path, (err: any, pixels: any) => {
-                var imgWidth = pixels.shape[0];
-                var imgHeight = pixels.shape[1];
-                parse(pixels.data, imgWidth, imgHeight);
-            });
-        } 
+
+        getPixels(this.path, (err: any, pixels: any) => {
+            var imgWidth = pixels.shape[0];
+            var imgHeight = pixels.shape[1];
+            parse(pixels.data, imgWidth, imgHeight);
+        });
     }
 
     private displayImg() {
@@ -33,6 +35,16 @@ class ViewManager {
         i.src = "img/test.png"//this.img.url;
         i.classList.add("parsedImg")
         document.body.appendChild(i);
+    }
+
+    SetFile(files: any) {
+        if(files.length > 0) {
+            this.path = files[0].path;
+        }
+    }
+
+    SetK(k: number) {
+        this.k = k;
     }
 }
 
